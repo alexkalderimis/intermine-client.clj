31bbdea39740391335a0408d8da7fdb77074b781
Alex Kalderimis <alexkalderimis@gmail.com>
2013-10-19 17:38:46 +0000

    Fix table defintion

--- src/intermine/client.clj
+++ src/intermine/client.clj
@@ -5,15 +5,25 @@
         intermine.client.model
         intermine.client.transport))
 
-(defn count [service query]
-  (eventually [model (get-model service)]
-      (let [xml (query-xml model query)
-            params {:query xml}]
-        (get-in-json service "/query/results" :count :type "count" :params params))))
+(defprotocol Closeable
+  (close [this]))
 
-(defn records [service query]
-  (eventually [model (get-model service)]
-      (let [xml (query-xml model query)
-            params {:query xml}]
-        (get-in-json service "/query/results" :results :type "objects" :params params))))
+(defn generate-def [func-name & {:keys [json path res-key] :or {path "results" res-key :results}}]
+    `(defn ~func-name
+      ~(format "Run a query against a service and return the %s" (or json path))
+      [service# query# & {:as opts#}]
+      (eventually [model# (get-model service#)]
+          (let [xml# (query-xml model# query#)
+                path# (str "/query/" ~path)
+                page# {:start (:offset opts#) :size (:limit opts#) } 
+                params# (merge page# (dissoc opts# :offset :limit) {:query xml#})]
+            (get-in-json service# path# ~res-key :type ~json :params params#)))))
 
+(defmacro def-results-fns [& fn-params]
+  (conj (for [param-set fn-params] (apply generate-def param-set)) `do))
+
+(def-results-fns
+  [rows]
+  [count :res-key :count :json "count"]
+  [records :res-key :results :json "objects"]
+  [table :res-key :results :path "results/tablerows"])
--- test/intermine/client/test/examples.clj
+++ test/intermine/client/test/examples.clj
@@ -1,17 +1,37 @@
 (ns intermine.client.test.examples
-  (:require intermine.client))
+  (:use intermine.client.futures)
+  (:require [intermine.client :as c]
+            [intermine.client.model :as m]))
 
 (use 'clojure.test)
+
+(declare service query attr-query)
+
+;; Testing that we are caching the model appropriately
+(defn report-model-requests [f]
+  (let [requests-before @m/model-requests]
+    (f)
+    (is 1 (- @m/model-requests requests-before))))
+
+(use-fixtures :once report-model-requests)
 
 (deftest queries
   (let [service {:base "http://www.flymine.org/query/service"}
         query   { :select ["Gene.transcripts"]
-                   :where  [[:lookup "forkhead"]]}]
-    (testing "Counting"
-        (is (= 4 @(intermine.client/count service query))))
-    (testing "Records"
-      (let [ [forkhead] @(intermine.client/records service query)
-             primId (-> forkhead :transcripts first :primaryIdentifier) ]
-        (is (= "FBtr0085321" primId))))))
-
-  
+                 :where  [[:lookup "forkhead"]]}
+        attr-query { :select ["Gene.transcripts.primaryIdentifier"]
+                    :where  [[:lookup "forkhead"]]}
+        transcript {:class "MRNA" :value "FBtr0085321"}]
+    @(eventually [n-transcripts (c/count service query) ;; Run in parallel.
+                  [forkhead] (c/records service query)
+                  [[cell]] (intermine.client/table service attr-query)
+                  [[primId] [sndPrimId]] (c/rows service attr-query)
+                  [[sndPrimId']] (c/rows service attr-query :offset 1)]
+                 (is (= 4 n-transcripts))
+                 (is (= (:value transcript) (-> forkhead
+                                                :transcripts first :primaryIdentifier)))
+                 (is (= sndPrimId sndPrimId'))
+                 (is (= (:value transcript)  primId))
+                 (is (not (= (:value transcript) sndPrimId')))
+                 (is (= transcript (select-keys cell (keys transcript))))
+                 )))
